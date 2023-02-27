6d5cc4f57f06e3f52feb6a12f5b84ae57048ddcc
prasanth-paul <126266948+prasanth-paul@users.noreply.github.com>
2023-02-27 08:01:30 -0800

    Update A-Z rows.c

--- A-Z rows.c
+++ A-Z rows.c
@@ -1,15 +1,15 @@
 //print A-Z rows pattern 
 #include<stdio.h>
-void main()
+int main()
 {
 	int i,j;
 	for(i=1;i<=26;i++)
 	{
 		for(j=1;j<=i;j++)
 	     {
-		   printf("%c",'A'+j-1);
+		   printf("%c ",'A'+j-1);
 	     }
 	     printf("\n");
-	     
+	  return 0;   
 	}
 }
