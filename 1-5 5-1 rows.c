e8ff7a7ce408615d81c37aa8893c388cf0cc3471
prasanth-paul <126266948+prasanth-paul@users.noreply.github.com>
2023-02-27 07:43:07 -0800

    Add files via upload

---
+++ 1-5 5-1 rows.c
@@ -0,0 +1,25 @@
+//print 1-5 & 5-1 rows pattern
+#include<stdio.h>
+int main()
+{
+	int i,j,rows;
+	printf("enter number of rows");
+	scanf("%d",&rows);
+	for(i=1;i<=rows;++i)
+	{
+		for(j=1;j<=i;++j)
+		{
+	        printf("%d ",j);
+          }
+	printf("\n");
+     }
+     for(i=rows;i>=1;--i)
+     {
+     	for(j=1;j<=i;++j)
+     {
+     	printf("%d ",j);
+	}
+	printf("\n");
+     }
+	return 0;
+}
